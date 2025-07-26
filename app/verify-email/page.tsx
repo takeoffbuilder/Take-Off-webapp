affda28b40709f4963722ff3d56098d725374188
takeoffbuilder <takeoffbuilder@gmail.com>
2025-07-25 21:48:44 -0400

    fixed form and added verify email page

---
+++ app/verify-email/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function VerifyEmailPage() {
+  return (
+    <div>
+      <h1>Verify Your Email</h1>
+      <p>Please check your inbox for a verification link.</p>
+    </div>
+  );
+}
